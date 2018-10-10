c93d8a2a2b66f3c3953d308210bfdc61f5261a8b
BlastoiseBlue <emmet@stanevich.com>
2018-10-10 12:18:24 -0400

    Added code to ChangeCalc and Project05

---
+++ cosc231/JavaScript/ChangeCalc.js
@@ -0,0 +1,10 @@
+function getChange(){
+    var cash=document.getElementById("cash");
+    var quarters=Math.floor(cash/25);
+    cash%=25;
+    var dimes=Math.floor(cash/10);
+    cash%=10;
+    var nickels=Math.floor(cash/5);
+    cash%=5;
+    var pennies=cash;
+}
