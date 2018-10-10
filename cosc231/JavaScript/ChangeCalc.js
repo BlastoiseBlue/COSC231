497ccc743ce1d3fa9f0df27e50f825fcbdf0e9a3
BlastoiseBlue <emmet@stanevich.com>
2018-10-10 16:25:32 -0400

    Finalized the change calculator

--- cosc231/JavaScript/ChangeCalc.js
+++ cosc231/JavaScript/ChangeCalc.js
@@ -1,15 +1,29 @@
 function getChange(){
     var cash=document.getElementById("cash").value;
+    var twenties=Math.floor(cash/20);
+    cash%=20;
+    var tens=Math.floor(cash/10);
+    cash%=10;
+    var fives=Math.floor(cash/5);
+    cash%=5;
+    var dollars=Math.floor(cash);
+    cash%=1;
+    cash*=100;
     var quarters=Math.floor(cash/25);
     cash%=25;
     var dimes=Math.floor(cash/10);
     cash%=10;
     var nickels=Math.floor(cash/5);
     cash%=5;
-    var pennies=cash;
-    var total = "Change back..."+"<br/>"+"Quarters: " + quarters
-    +"<br/>"+" Dimes: "+dimes
-    +"<br/>"+"Nickels: "+nickels
-    +"<br/>"+"Pennies: "+pennies;
+    var pennies=Math.floor(cash);
+    var total = "Change back..."
+        +"<br/>"+"Twenties: "+twenties
+        +"<br/>"+"Tens: "+tens
+        +"<br/>"+"Fives: "+fives
+        +"<br/>"+"Ones: "+dollars
+        +"<br/>"+"Quarters: " + quarters
+        +"<br/>"+" Dimes: "+dimes
+        +"<br/>"+"Nickels: "+nickels
+        +"<br/>"+"Pennies: "+pennies;
     document.getElementById("outputTotal").innerHTML=total;
 }
