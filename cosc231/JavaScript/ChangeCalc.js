ab122703b4a06b77b9d55c9442de3ace8d4bee0e
BlastoiseBlue <emmet@stanevich.com>
2018-10-10 16:07:48 -0400

    Added line breaks to the text

--- cosc231/JavaScript/ChangeCalc.js
+++ cosc231/JavaScript/ChangeCalc.js
@@ -7,9 +7,9 @@ function getChange(){
     var nickels=Math.floor(cash/5);
     cash%=5;
     var pennies=cash;
-    var total = "Change back..."+"\n"+"Quarters: " + quarters
-    +"\n"+" Dimes: "+dimes
-    +"\n"+"Nickels: "+nickels
-    +"\n"+"Pennies: "+pennies;
+    var total = "Change back..."+"<br/>"+"Quarters: " + quarters
+    +"<br/>"+" Dimes: "+dimes
+    +"<br/>"+"Nickels: "+nickels
+    +"<br/>"+"Pennies: "+pennies;
     document.getElementById("outputTotal").innerHTML=total;
 }
