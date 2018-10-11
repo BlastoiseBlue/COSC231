520816d1bc7cf12184c5da011ad292de8b275dc6
BlastoiseBlue <emmet@stanevich.com>
2018-10-11 14:51:59 -0400

    Added console output to GuessingGame.js

--- cosc231/JavaScript/GuessingGame.js
+++ cosc231/JavaScript/GuessingGame.js
@@ -1,7 +1,9 @@
-var x=Math.floor(Math.random()*100);
+var x;
 function scramble(){
     x=Math.floor(Math.random()*100);
+    console.log(x);
 }
+scramble();
 function guess(){
     var guess=document.getElementById("guess").value;
     var name=document.getElementById("name").value;
