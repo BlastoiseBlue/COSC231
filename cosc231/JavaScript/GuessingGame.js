be80fcc5be311b19c137bb0dc9ac05eb70f44104
BlastoiseBlue <emmet@stanevich.com>
2018-10-11 13:47:27 -0400

    Finished GuessingGame and implemented it into Project05

--- cosc231/JavaScript/GuessingGame.js
+++ cosc231/JavaScript/GuessingGame.js
@@ -1,4 +1,16 @@
 var x=Math.floor(Math.random()*10);
+function scramble(){
+    x=Math.floor(Math.random()*10);
+}
 function guess(){
-
+    var guess=document.getElementById("guess").value;
+    var result;
+    if(guess>x)result="Sorry "+document.getElementById("name").value+", but your guess was too high";
+    else if(guess<x)result="Sorry "+document.getElementById("name").value+", but your guess was too low";
+    else if(guess==x){
+        result="Congratulations "+document.getElementById("name").value+"!"+"<br/>"
+            +"Enter a new number to try again!";
+        scramble();
+    }
+    else result="ERROR";
 }
