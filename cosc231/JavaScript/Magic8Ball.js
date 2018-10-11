d1fb1347c86b50ca4481bd60a7ae2d4342c4615e
BlastoiseBlue <emmet@stanevich.com>
2018-10-11 16:57:55 -0400

    Added code to Magic 8 ball

--- cosc231/JavaScript/Magic8Ball.js
+++ cosc231/JavaScript/Magic8Ball.js
@@ -1,3 +1,20 @@
-function predict(){
+import * as Papa from "papaparse";
 
+var responses=Papa.parse("../cosc231/CSV/Responses.csv", {
+    complete:function (results) {
+        console.log(results);
+    }
+});
+var x;
+function roll(){
+    x=Math.floor(Math.random()*responses.length);
+}
+roll();
+function predict(){
+    var result=responses[x];
+    var n=document.getElementById("name");
+    var q=document.getElementById("question");
+    document.getElementById("answer").innerHTML="Hi, "+n+"! The answer to \""+q+"\" is:"
+    +"<br/>"+result;
+    roll();
 }
