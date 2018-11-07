75a764ebe99ce0793236508a712632590dcf290b
BlastoiseBlue <emmet@stanevich.com>
2018-11-07 14:32:52 -0500

    Created "HelloWorld.php"

---
+++ cosc231/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+$txt = "Hello World!";
+$firstName = "Emmet";
+$lastName = "Stanevich";
+echo "<h1>Hello World!</h1>";
+echo "<p>My name is: ".$firstName." ".$lastName."</p>";
+/**
+ * Created by PhpStorm.
+ * User: emmet
+ * Date: 11/6/2018
+ * Time: 2:41 PM
+ */
+?>
