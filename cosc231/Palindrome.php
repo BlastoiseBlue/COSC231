256a675103c9296ec8e49c0c3ef438a3e6616d6c
BlastoiseBlue <emmet@stanevich.com>
2018-11-14 11:22:28 -0500

    Created Palindrome program
    Merely a shell at the moment, this program will take inputs via a field and displays them, and says if each one is a palindrome.

---
+++ cosc231/Palindrome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Created on 11/14 by Emmet Stanevich and Ben Kolar on 11/14
+This program accepts strings, and then shows whether each one is a palindrome
+-->
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Palindrome</title>
+    <link rel="stylesheet" href="../styles/stylesheet3.css">
+</head>
+<body>
+<div class="main">
+    <?php
+    
+    ?>
+</div>
+</body>
+</html>
