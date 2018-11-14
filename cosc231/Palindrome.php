1f35f82e1a13fdd8b27796b1f98f5e5223704fdc
BlastoiseBlue <emmet@stanevich.com>
2018-11-14 12:27:03 -0500

    Added main code for the palindrome checker
    It still needs to remember the previous entries

--- cosc231/Palindrome.php
+++ cosc231/Palindrome.php
@@ -13,7 +13,38 @@ This program accepts strings, and then shows whether each one is a palindrome
 <body>
 <div class="main">
     <?php
-    
+    $stringsErr="";
+    global $strings;
+    $strings=array();
+    echo "<h1>Palindrome Detector</h1><h2>This program will accept strings and tell you if they are palindromes</h2>";
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        if(empty($_POST["input"]))$stringsErr="Please enter a string";
+        else{
+            array_push($strings,test_input(($_POST["input"])));
+        }
+    }
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" autocomplete="off">
+        <span>Please enter a word or phrase, case and spaces will be ignored: </span><input type="text" name="input">
+        <span class="error">* <?php echo $stringsErr;?></span>
+        <br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+    array_walk($strings,"compare");
+    function compare($value){
+        echo "<span>".$value." is ";
+        $placeholder=implode("",explode(" ,.!?",$value));
+        if($placeholder==strcasecmp($placeholder,strrev($placeholder)))echo "a palindrome!</span>";
+        else echo "not a palindrome!</span>";
+    }
     ?>
 </div>
 </body>
