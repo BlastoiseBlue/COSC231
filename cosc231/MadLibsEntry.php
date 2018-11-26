81a45c9d43205558142c146124d2c67aa0d0f695
BlastoiseBlue <emmet@stanevich.com>
2018-11-26 12:41:11 -0500

    Created the input page for the Mad Libs
    It takes 8 strings and places them into the storyline.

--- cosc231/MadLibsEntry.php
+++ cosc231/MadLibsEntry.php
@@ -14,7 +14,22 @@ This program will take 8 strings via forms and fit them into a Mad Lib on a sepa
 </head>
 <body>
 <div class="main">
-
+    <?php
+    $_SESSION["Entries"]=array();
+    echo "<h1>Mad Libs</h1><h2>Please fill out the forms with something fitting</h2>";
+    $categories=array("Residence","Name","Profession","Superior","Task","Object","Time of day","Reward");
+    ?>
+    <form method="post" action="MadLibs.php">
+        <table class="shop">
+            <tr><th>Prompt</th><th>Input</th></tr>
+            <?php
+            for($i=0;$i<8;$i++){
+                echo "<tr><td>"."$categories[$i]".":</td><td><input type='text' name='entry".$i."'></td>";
+            }
+            ?>
+        </table>
+        <input type="submit" name="submit" value="Submit">
+    </form>
 </div>
 </body>
 </html>
